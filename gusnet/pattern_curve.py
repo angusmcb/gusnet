a2e4de6778871d0d4f31f107dc3969a07798d612
angusmcb <git@zog.mozmail.com>
2025-11-12 22:59:27 +0100

    Minor improvements in Pattern

--- gusnet/pattern_curve.py
+++ gusnet/pattern_curve.py
@@ -4,16 +4,23 @@ from collections.abc import Iterable
 
 
 class Pattern(tuple):
-    def __new__(cls, pattern: Iterable[float] | str | None = None):
+    def __new__(cls, pattern: Iterable | str | None = None):
         if pattern is None:
             return super().__new__(cls)
 
         pattern_parts = pattern.strip().split() if isinstance(pattern, str) else pattern
 
         try:
-            return super().__new__(cls, (float(item) for item in pattern_parts))
+            return super().__new__(cls, [float(item) for item in pattern_parts])
         except (ValueError, TypeError):
             raise ValueError(pattern) from None
 
     def __str__(self) -> str:
         return " ".join(map(str, self))
+
+    @classmethod
+    def factory(cls, pattern: Iterable[float] | str | None = None) -> Pattern | None:
+        if pattern_class := cls(pattern):
+            return pattern_class
+        else:
+            return None
