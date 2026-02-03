2cc68be6f9334aebf823e558020a9753adcdaa27
Angus McBride <git@zog.mozmail.com>
2026-02-03 21:31:49 +0100

    Self not available in older pythons

--- gusnet/pattern_curve.py
+++ gusnet/pattern_curve.py
@@ -2,7 +2,10 @@ from __future__ import annotations
 
 import ast
 from collections.abc import Iterable
-from typing import ClassVar, Self, SupportsFloat
+from typing import TYPE_CHECKING, ClassVar, SupportsFloat
+
+if TYPE_CHECKING:
+    from typing import Self
 
 
 class Pattern(str):
